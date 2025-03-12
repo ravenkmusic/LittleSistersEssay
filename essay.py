2fb68c72f9cdcba1c30c35799b8091d1efac9d18
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-12 16:23:15 -0400

    removed semi colons

--- essay.py
+++ essay.py
@@ -8,7 +8,7 @@ def check_sentence_ending(sentence):
     return sentence.endswith(".")
 
 def clean_up_spacing(sentence):
-    return sentence.strip();
+    return sentence.strip()
 
 def replace_word_choice(sentence, old_word, new_word):
-    return sentence.replace(old_word, new_word);
+    return sentence.replace(old_word, new_word)
