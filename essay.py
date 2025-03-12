a32d55d6bb07e90b695233c714e7ceac0d9a1d84
Raven Hendricks <ravenkbeats@gmail.com>
2025-03-12 16:14:19 -0400

    created title function

--- essay.py
+++ essay.py
@@ -2,14 +2,7 @@
 
 
 def capitalize_title(title):
-    """Convert the first letter of each word in the title to uppercase if needed.
-
-    :param title: str - title string that needs title casing.
-    :return: str - title string in title case (first letters capitalized).
-    """
-
-    return title.capitalize();
-
+    return title.title();
 
 def check_sentence_ending(sentence):
     """Check the ending of the sentence to verify that a period is present.
